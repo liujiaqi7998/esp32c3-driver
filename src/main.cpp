c9b8de6c63e2f9bf6e9dad06b3dae181a6264c69
liujiaqi7998 <1792498113@qq.com>
2024-04-27 15:41:09 +0800

    提交

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,8 @@ Encoder BEnc(12, 18);
 float AInput = 0, AOutput = 0, ASetpoint = 0;
 float BInput = 0, BOutput = 0, BSetpoint = 0;
 float Kp = 0.5, Ki = 1.5, Kd = 0;
-float OutputLimits_MAX = 177.0;
+float AOutputLimits_MAX = 60;
+float BOutputLimits_MAX = 56;
 QuickPID APID(&AInput, &AOutput, &ASetpoint);
 QuickPID BPID(&BInput, &BOutput, &BSetpoint);
 
@@ -24,12 +25,12 @@ void setup()
 
   ASetpoint = 30.0;
   APID.SetTunings(Kp, Ki, Kd);
-  APID.SetOutputLimits(0, OutputLimits_MAX);
+  APID.SetOutputLimits(0, AOutputLimits_MAX);
   APID.SetMode(APID.Control::automatic);
 
-  BSetpoint = 40.0;
+  BSetpoint = 30.0 ;
   BPID.SetTunings(Kp, Ki, Kd);
-  BPID.SetOutputLimits(0, OutputLimits_MAX);
+  BPID.SetOutputLimits(0, BOutputLimits_MAX);
   BPID.SetMode(BPID.Control::automatic);
 
   Serial.println("Setup success");
@@ -40,12 +41,13 @@ void loop()
 {
   AInput = (double)AEnc.readAndReset();
   BInput = (double)-BEnc.readAndReset();
+
   APID.Compute();
   BPID.Compute();
-   Serial.printf("%lf,%lf,%lf,%lf\n", AInput, BInput, AOutput, BOutput);
-  // set_A_pwm(map(AOutput + 28, 0, OutputLimits_MAX, 0, Amplitude));
-  set_B_pwm(map(BOutput, 0, OutputLimits_MAX, 0, Amplitude));
-  delay(15);
+  // Serial.printf("%lf,%lf,%lf,%lf\n", AInput, BInput, AOutput, BOutput);
+  set_A_pwm(map(AOutput, 0, AOutputLimits_MAX, 0, Amplitude));
+  set_B_pwm(map(BOutput, 0, BOutputLimits_MAX, 0, Amplitude));
+  delay(10);
 
 }
 
